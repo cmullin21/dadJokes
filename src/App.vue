cb3990584bbcafeda6e23588e75a556d44c81d85
Cody Mullin <cmullin21@gmail.com>
2020-03-25 12:32:32 -0400

    Added icon for scroll

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <full-page :options="options" id="fullpage" v-if="isLoaded">
     <div v-for="dadJoke in dadJokes" :key="dadJoke.data.id" class="section">
-      <div class="container flex flex-col">
-        <img src="./assets/dadjokes.png" class="h-48 w-48 mx-auto mt-56">
-        <p class="text-center text-2xl">{{ dadJoke.data.title }}</p>
-        <p class="text-center text-2xl">{{ dadJoke.data.selftext }}</p>
-        <div>
-          <div class="text-center pt-16">Author: {{ dadJoke.data.author_fullname }}</div>
-          <div class="text-center">{{ dadJoke.data.ups }} Upvotes | {{ dadJoke.data.num_comments }} Comments</div>
-        </div>
+      <div class="mobileContainer md:container flex flex-col">
+        <img src="./assets/dadjokes.png" class="h-48 w-48 mx-auto mt-64">
+          <p class="text-center text-2xl">{{ dadJoke.data.title }}</p>
+          <p class="text-center text-2xl">{{ dadJoke.data.selftext }}</p>
+          <div>
+            <div class="text-center pt-16">Author: {{ dadJoke.data.author }}</div>
+            <div class="text-center">{{ dadJoke.data.ups }} Upvotes | {{ dadJoke.data.num_comments }} Comments</div>
+          </div>
+          <div class="flex flex-col mt-64">
+            <svg fill="currentColor" viewBox="0 0 20 20" class="w-8 h-8 mx-auto"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </div>
         </div>
       </div>
     </full-page>
@@ -46,6 +49,12 @@ export default {
 <style>
   .container{
     width: 90%;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  .mobileContainer{
+    width: 80%;
     margin-right: auto;
     margin-left: auto;
   }
